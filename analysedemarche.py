7cee8348897922a2ef381dd6523eb2884b1718cd
souiai <merge.tool>
2022-06-04 22:24:06 +0200

    organiser les fonctions

--- analysedemarche.py
+++ analysedemarche.py
@@ -4,35 +4,72 @@ from math import *
 import csv
 
 
-#details de chaque livre
-t=1 # parcourir les categories
-y=0 # parcourir les livres
-#def detailsBooks():
-category = categoryBooks(t) # le parametre t correspond aux classements des categories c'est pour parcourir tous les categories
-titleCategory = extractTitleCategory() # liste des tous les titres des categories
-titleCategory=extracTitleCategory[y]
 
-url=category[y]             # y correspond au page de category en cours
-reponsePageBook = requests.get(url)
-pageBook = reponsePageBook.content
-soupBook = BeautifulSoup(pageBook,"html.parser")
+#page index de chaque categorie ==> parcourir tout les lien des categories
+def pageIndexForCategory():
+    ul=[]
+    linksCategory=[]
+    pageIndexCategory=[]
 
-title = soupBook.find('h1')
-titleBook=title.string
+    url='http://books.toscrape.com/index.html'
+    reponseCategory = requests.get(url)
+    pageCategory = reponseCategory.content
+    soup = BeautifulSoup(pageCategory,"html.parser")
+    ul = soup.find_all('ul')
+    linksCategory = ul[2].find_all('a')
 
-paragraph=soupBook.find_all('p')
-description=paragraph[3].string
+    for urlcat in linksCategory:
+        adress='http://books.toscrape.com/'+urlcat['href']
+        pageIndexCategory.append(adress)
+        
+    return pageIndexCategory
 
-td=soupBook.find_all('td')
-universal_product_code=td[0].string
-price_including_tax=td[2].string
-price_excluding_tax=td[3].string
-number_available=td[5].string
-review_rating=td[6].string
 
-image=soupBook.find('img')
-src_image=url+'../../'+image['src'] 
+
+#def etlCategoryBooks(linksCategory):#le lien de chaque livre ==> parcourir tout les lien des livre
+def transformLinkPageCategory(): #transformer tous les liens des categorie
     
+    pageIndexCategory=pageIndexForCategory() #contient tous les page index des categorie
+    pageUrl=[]
+    numberPageCategory=[]
+    nombreCategory=len(pageIndexCategory)
+    
+    for index in range(nombreCategory):
+       
+        newUrl=pageIndexCategory[index].replace("index.html","")
+        pageUrl.append(newUrl)
+        
+    return pageUrl
+
+
+
+def etlNumberOfBooks(link):#le nombre des livres de chaque categorie 
+    urlNumberPage=link
+    reponseNumberPage=requests.get(urlNumberPage)
+    soupNumberPage = BeautifulSoup(reponseNumberPage.content,'html.parser')
+    strongNumberPage = soupNumberPage.find_all('strong')
+    numberOfBooks=strongNumberPage[1].text
+    return int(numberOfBooks)
+
+
+
+def numberPagePerCategory():
+    
+    pageIndexCategory=pageIndexForCategory() #contient tous les page index des categorie
+    numberPageCategory=[]
+    #extraire les url des categories qui on 2 page ou plus
+    nombreCategory=len(pageIndexCategory)
+    for index in range(nombreCategory):
+        NumberOfBooks = etlNumberOfBooks(pageIndexCategory[index])
+        if NumberOfBooks>20:# 20==>nombre livrepar page NumberOfBooks==> nombre de livre/categorie
+            numberPage = ceil(NumberOfBooks/20)
+            numberPageCategory.append(numberPage)
+        else:
+            onePage=0
+            numberPageCategory.append(0)
+        
+    return numberPageCategory
+
 
 
 def categoryBooks(t):
@@ -61,31 +98,9 @@ def categoryBooks(t):
                 a=book.find('a')
                 href=url+'../../'+a['href']
                 links.append(href)
-    
     return links
 
 
-
-
-#page index de chaque categorie ==> parcourir tout les lien des categories
-def pageIndexForCategory():
-    ul=[]
-    linksCategory=[]
-    pageIndexCategory=[]
-
-    url='http://books.toscrape.com/index.html'
-    reponseCategory = requests.get(url)
-    pageCategory = reponseCategory.content
-    soup = BeautifulSoup(pageCategory,"html.parser")
-    ul = soup.find_all('ul')
-    linksCategory = ul[2].find_all('a')
-
-    for urlcat in linksCategory:
-        adress='http://books.toscrape.com/'+urlcat['href']
-        pageIndexCategory.append(adress)
-        
-    return pageIndexCategory
-#pageIndexForCategory()
 
 def extractTitleCategory():
     linkCategory=pageIndexForCategory()
@@ -100,48 +115,62 @@ def extractTitleCategory():
 
 
 
-#def etlCategoryBooks(linksCategory):#le lien de chaque livre ==> parcourir tout les lien des livre
-def transformLinkPageCategory(): #transformer tous les liens des categorie
-    
-    pageIndexCategory=pageIndexForCategory() #contient tous les page index des categorie
-    pageUrl=[]
-    numberPageCategory=[]
-    nombreCategory=len(pageIndexCategory)
-    
-    for index in range(nombreCategory):
-       
-        newUrl=pageIndexCategory[index].replace("index.html","")
-        pageUrl.append(newUrl)
-        
-    return pageUrl
+#details de chaque livre
+t=0 # parcourir les categories
+y=0 # parcourir les livres
+
+donneeLivre=[]
+
+srcImage=[]
+entete=['category','product_page_url','title','product_description','universal_ product_code','price_including_tax',
+        'price_excluding_tax','number_available','review_rating']
+
+category = categoryBooks(t) # le parametre t correspond aux classements des categories c'est pour parcourir tous les categories
+
+extracTitleCategory = extractTitleCategory()
+titleCategory=extracTitleCategory[y]
+
+donneeLivre.append(titleCategory)
+
+url=category[y]        # y correspond au page de category en cours
+reponsePageBook = requests.get(url)
+pageBook = reponsePageBook.content
+soupBook = BeautifulSoup(pageBook,"html.parser")
+
+donneeLivre.append(url)
+
+title = soupBook.find('h1')
+titleBook=title.string
+donneeLivre.append(titleBook)
+
+paragraph=soupBook.find_all('p')
+description=paragraph[3].string
+donneeLivre.append(description)
+
+td=soupBook.find_all('td')
+universal_product_code=td[0].string
+donneeLivre.append(universal_product_code)
 
 
-def numberPagePerCategory():
-    
-    pageIndexCategory=pageIndexForCategory() #contient tous les page index des categorie
-    numberPageCategory=[]
-    #extraire les url des categories qui on 2 page ou plus
-    nombreCategory=len(pageIndexCategory)
-    for index in range(nombreCategory):
-        NumberOfBooks = etlNumberOfBooks(pageIndexCategory[index])
-        if NumberOfBooks>20:# 20==>nombre livrepar page NumberOfBooks==> nombre de livre/categorie
-            numberPage = ceil(NumberOfBooks/20)
-            numberPageCategory.append(numberPage)
+price_including_tax=td[2].string 
+donneeLivre.append(price_including_tax)
 
-        else:
-            onePage=0
-            numberPageCategory.append(0)
-        
-    return numberPageCategory
+price_excluding_tax=td[3].string  
+donneeLivre.append(price_excluding_tax)
 
+number_available=td[5].string
+donneeLivre.append(number_available)    
 
-def etlNumberOfBooks(link):#le nombre des livres de chaque categorie 
-    urlNumberPage=link
-    reponseNumberPage=requests.get(urlNumberPage)
-    soupNumberPage = BeautifulSoup(reponseNumberPage.content,'html.parser')
-    strongNumberPage = soupNumberPage.find_all('strong')
-    numberOfBooks=strongNumberPage[1].text
-    return int(numberOfBooks)
-#print(etlNumberOfBooks(PIC[12]))
+review_rating=td[6].string   
+donneeLivre.append(review_rating)
 
-    
+image=soupBook.find('img')
+src_image=url+'../../'+image['src']
+srcImage.append(src_image)
+
+with open("etl.csv","a") as fichier:
+    writer = csv.writer(fichier,delimiter=',')
+    writer.writerow(entete)
+    for details in donneeLivre:
+        ligne = [details]
+        writer.writerow(ligne)
